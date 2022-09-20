508c64fa12aa48274ef10542ea8dbd79bcb95160
Ethan Wall <53667981+EthanWall@users.noreply.github.com>
2022-09-19 22:13:57 -0400

    Added necessary run() line

--- robot.py
+++ robot.py
@@ -4,12 +4,16 @@ import wpilib.drive
 class Robot(wpilib.TimedRobot):
     
     def robotInit(self) -> None:
+        # Motors
         fl_motor = wpilib.Spark(0)
         rl_motor = wpilib.Spark(1)
         fr_motor = wpilib.Spark(2)
         rr_motor = wpilib.Spark(3)
+
+        # A drive base. Combines motors into one simple interface
         self.drive = wpilib.drive.MecanumDrive(fl_motor, rl_motor, fr_motor, rr_motor)
         
+        # USB Controller
         self.stick = wpilib.XboxController(0)
 
     def teleopPeriodic(self) -> None:
@@ -20,3 +24,6 @@ class Robot(wpilib.TimedRobot):
 
         # Move the robot chassis
         self.drive.driveCartesian(fwd, right, rot)
+
+# Start the robot code
+wpilib.run(Robot)
